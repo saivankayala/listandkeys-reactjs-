721c60b79a4e7d64908ed85e856fa21e1b1b6ecd
Sai Vankayala <saivankalaya@gmail.com>
2023-08-01 07:02:18 +0000

    first commit

---
+++ src/App.js
@@ -0,0 +1,41 @@
+import UserProfile from './components/UserProfile/index'
+import './App.css'
+
+const userdetailsList = [
+  {
+    uniqueNo: 1,
+    imgUrl: 'https://assets.ccbp.in/frontend/react-js/esther-howard-img.png',
+    name: 'sai',
+    role: 'fullstack Developer',
+  },
+  {
+    uniqueNo: 2,
+    imgUrl: 'https://assets.ccbp.in/frontend/react-js/esther-howard-img.png',
+    name: 'sai',
+    role: 'fullstack Developer',
+  },
+  {
+    uniqueNo: 3,
+    imgUrl: 'https://assets.ccbp.in/frontend/react-js/esther-howard-img.png',
+    name: 'sai',
+    role: 'fullstack Developer',
+  },
+  {
+    uniqueNo: 4,
+    imgUrl: 'https://assets.ccbp.in/frontend/react-js/esther-howard-img.png',
+    name: 'sai',
+    role: 'fullstack Developer',
+  },
+]
+const App = () => (
+  <div className="list-container">
+    <h1 className="title">Users Lists</h1>
+    <ul>
+      {userdetailsList.map(each => (
+        <UserProfile userdetails={each} key={each.uniqueNo} />
+      ))}
+    </ul>
+  </div>
+)
+
+export default App
